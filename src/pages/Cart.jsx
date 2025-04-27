3e9f72e74e0a5fd8b773561a61214e64f7c1665e
Abhisek Sikdar <abhiseksikdar40@gmail.com>
2025-04-27 22:49:19 +0530

    feat: Added More Features.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,56 @@
 export default function Cart () {
-    
+    return (
+        <>
+            <div style={{backgroundColor: "#e6f1f1"}}>
+                <div className="container py-3">
+                    <h2 className="text-center mb-4">My Cart</h2>
+                    <div className="d-flex justify-content-evenly">
+                    <div className="card mb-3" style={{width: "40%"}}>
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                        <img src="https://placehold.co/350X650" className="img-fluid rounded-start" alt="..."/>
+                        </div>
+                        <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">Product Name</h5>
+                           <div className="card-text">
+                           <span className="fw-bold">Discounted Price</span> <span>#MRP#</span>
+                <p>Discount Percentage</p>
+                <span className="fw-bold">Quatity:</span> <span><button>-</button> <input type="number" name="" id="" style={{width: "30px"}} /> <button>+</button></span><br/>
+                <button className='btn btn-danger mt-5' style={{borderRadius: "0px", width: "255px"}}>Remove From Cart</button>
+                <button className='btn btn-outline-primary mt-2' style={{borderRadius: "0px", width: "255px"}}>Move To Wishlist</button>
+                           </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="bg-light px-5">
+                        <h4 className="justify-content-start mt-3">Price Details</h4>
+                        <div className="border border-1"></div>
+                        <div className="d-flex justify-content-between">
+                            <p>price(1 item)</p>
+                            <p>$200</p>
+                        </div>
+                        <div className="d-flex justify-content-between">
+                            <p>Discount</p>
+                            <p>-$50</p>
+                        </div>
+                        <div className="d-flex justify-content-between">
+                            <p>Delivery Charges</p>
+                            <p>$5</p>
+                        </div>
+                        <div className="border border-1"></div>
+                        <div className="d-flex justify-content-between mt-1">
+                            <p className="fw-bold">Total Amount</p>
+                            <p>$155</p>
+                        </div>
+                        <div className="border border-1"></div>
+                        <p>You will save $50 on this order</p>
+                        <button className='btn btn-success my-3' style={{borderRadius: "0px", width: "255px", height: "40px"}}>Place Order</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -7,21 +7,6 @@ export default function Wishlist () {
         <h4 className="text-center fw-bold my-3">My Wishlist</h4>
         <div className='container my-5' style={{backgroundColor: "#f2f8f8", padding: "30px"}}>
             <div className="row">
-                <div className="col-md-3">
-                <div className="card" style={{width: "255px", borderRadius: "0px"}}>
-               <div className='position-relative'>
-                <button style={{background: "transparent", border: "none" }} className='position-absolute top-0 end-0 p-2'><img style={{height: "32px", backgroundColor: "#FFFFFF", borderRadius: "17px", padding: "5px 4px 2px 4px"}} src="/favorite-red.png" alt="wishlist" /></button>
-               <img style={{borderRadius: "0px"}} src="https://placehold.co/150" className="card-img-top" alt="..."/>
-               </div>
-                <div className="card-body text-center">
-                    <h5 className="card-title">Product Name</h5>
-                    <p className="card-text">$Price</p>
-                </div>
-                </div>
-                <button className='btn btn-primary' style={{borderRadius: "0px", width: "255px"}}>Move To Cart</button>
-            </div>
-
-
             <div className="col-md-3">
                 <div className="card" style={{width: "255px", borderRadius: "0px"}}>
                <div className='position-relative'>
@@ -34,36 +19,7 @@ export default function Wishlist () {
                 </div>
                 </div>
                 <button className='btn btn-primary' style={{borderRadius: "0px", width: "255px"}}>Move To Cart</button>
-            </div>
-
-
-            <div className="col-md-3">
-                <div className="card" style={{width: "255px", borderRadius: "0px"}}>
-               <div className='position-relative'>
-                <button style={{background: "transparent", border: "none" }} className='position-absolute top-0 end-0 p-2'><img style={{height: "32px", backgroundColor: "#FFFFFF", borderRadius: "17px", padding: "5px 4px 2px 4px"}} src="/favorite-red.png" alt="wishlist" /></button>
-               <img style={{borderRadius: "0px"}} src="https://placehold.co/150" className="card-img-top" alt="..."/>
-               </div>
-                <div className="card-body text-center">
-                    <h5 className="card-title">Product Name</h5>
-                    <p className="card-text">$Price</p>
-                </div>
-                </div>
-                <button className='btn btn-primary' style={{borderRadius: "0px", width: "255px"}}>Move To Cart</button>
-            </div>
-
-
-            <div className="col-md-3">
-                <div className="card" style={{width: "255px", borderRadius: "0px"}}>
-               <div className='position-relative'>
-                <button style={{background: "transparent", border: "none" }} className='position-absolute top-0 end-0 p-2'><img style={{height: "32px", backgroundColor: "#FFFFFF", borderRadius: "17px", padding: "5px 4px 2px 4px"}} src="/favorite-red.png" alt="wishlist" /></button>
-               <img style={{borderRadius: "0px"}} src="https://placehold.co/150" className="card-img-top" alt="..."/>
-               </div>
-                <div className="card-body text-center">
-                    <h5 className="card-title">Product Name</h5>
-                    <p className="card-text">$Price</p>
-                </div>
-                </div>
-                <button className='btn btn-primary' style={{borderRadius: "0px", width: "255px"}}>Move To Cart</button>
+                <button className='btn btn-danger mt-2' style={{borderRadius: "0px", width: "255px"}}>Remove</button>
             </div>
             </div>
         </div>
